335e7fcfa4af3a85035c0a39f199979a94df1ce1
Mehedi Hasan Hasib <mehedi.mh50@gmail.com>
2024-05-08 18:44:03 +0600

    web-socket added, event is fireing

---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Events\TestEvent;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use PHPUnit\Util\Test;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Route::get('event', function () {
+    event(new TestEvent());
+});
+
+
+Route::prefix('/admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('admin.admin-dashboard');
+    });
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
